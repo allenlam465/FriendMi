8b45c23f1b2ab626e3b87c46d658375b0b332ab8
Allen Lam <allenlam465@gmail.com>
2018-02-03 20:41:36 -0800

    Working UI

--- app/src/main/java/com/friendmi/friendmi/FBlogin.java
+++ app/src/main/java/com/friendmi/friendmi/FBlogin.java
@@ -1,21 +1,9 @@
 package com.friendmi.friendmi;
 
-import android.app.Application;
-import com.facebook.FacebookSdk;
-import com.facebook.drawee.backends.pipeline.Fresco;
-
 /**
  * Created by alam on 2/3/18.
  */
 
-public class FBlogin extends Application {
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        FacebookSdk.sdkInitialize(getApplicationContext());
-
-        Fresco.initialize(this);
-    }
+public class FBlogin {
 
 }
